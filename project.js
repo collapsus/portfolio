8c2c96ebe1b5b299477f790aba30da293d555882
Alexey Berezhnoy <collapsus@gmail.com>
2010-04-06 17:48:51 +0400

    первый коммит, набросок списка проэктов (сделан на подобие couplelog-а)

---
+++ project.js
@@ -0,0 +1,48 @@
+function Project(projectCollection, data){
+    this.projectCollection = projectCollection;
+    this.projectCollection.body = $('body');
+    this.data = data;
+    this.makeDOM();
+}
+
+Project.prototype.makeHTML = function(){
+    return [
+        '<div class="project">',
+            '<h2 class="title"><span>', this.data.title,'</span></h2>',
+            '<div class="slide"',
+                '<div class="about">',
+                    this.data.about,
+                '</div>',
+                '<div class="example-url">',
+                    '<a href="', this.data.exampleUrl, '">Перейти к примеру</a>',
+                '</div>',
+                '<div class="source-url">',
+                    '<a href="', this.data.sourceUrl, '">Посмотреть исходный код</a>',
+                '</div>',
+                '<div class="widget-url">',
+                    '<a href="', this.data.widgetUrl, '">Установить Яндекс виджет</a>',
+                '</div>',
+            '</div>',
+        '</div>'
+    ].join('');
+};
+
+Project.prototype.makeDOM = function(){
+    this.elem = $(this.makeHTML()).appendTo(this.projectCollection.body);
+    var _this = this;
+    $.each({
+        title: '.title',
+        slide: '.slide',
+        about: '.about',
+        exampleUrl: '.example-url',
+        sourceUrl: '.source-url',
+        widgetUrl: '.widget-url'
+    }, function(k, v){
+        _this[k] = _this.elem.find(v);
+    });
+
+    this.title.click(function(){
+        _this.projectCollection.makeCurrent(this);
+    });
+
+};
---
+++ projectcollection.js
@@ -0,0 +1,25 @@
+function ProjectCollection(projectsData){
+    this.projectsData = projectsData;
+    this.projects = {};
+    this.makeDOM();
+}
+
+ProjectCollection.prototype.makeDOM = function(){
+    var _this = this;
+    $.each(_this.projectsData, function(k, v){
+        _this.projects[k] = new Project(_this, v);
+    });
+};
+
+ProjectCollection.prototype.makeCurrent = function(title){
+    var _this = this;
+    $.each(_this.projectsData, function(k, v){
+        if (_this.projects[k].title[0] == title) {
+            _this.projects[k].slide.slideToggle("fast");
+            _this.projects[k].title.toggleClass("active");
+        } else {
+            _this.projects[k].slide.slideUp("fast");
+            _this.projects[k].title.removeClass("active");
+        }
+    });
+};
